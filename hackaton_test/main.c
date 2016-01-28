948a4a143703c101303a97ff2a04d9cb260ad648
Didir19 <adir.gozlan@gmail.com>
2016-01-28 21:52:47 +0200

    Initial Commit

---
+++ hackaton_test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hackaton_test
+//
+//  Created by Adir Gozlan on 28/01/16.
+//  Copyright © 2016 Adir Gozlan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
